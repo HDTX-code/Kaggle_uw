c26ff80a1adf6ddc87ea9b8eb334880b6a8e1e32
HDTX-code <1252900424@qq.com>
2022-05-11 16:17:03 +0800

    增加预测模块

--- predict.py
+++ predict.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='训练参数设置')
     parser.add_argument('--backbone', type=str, default='vgg', help='特征网络选择，默认resnet50')
     parser.add_argument('--model_path', type=str, required=True, help='模型参数位置')
-    parser.add_argument('--num_classes', type=int, default=2, help='种类数量 + 1')
+    parser.add_argument('--num_classes', type=int, default=4, help='种类数量 + 1')
     parser.add_argument('--w', type=int, default=512, help='宽')
     parser.add_argument('--h', type=int, default=512, help='高')
     parser.add_argument('--mix_type', type=int, default=0, help='原图与生成的图进行混合模式')

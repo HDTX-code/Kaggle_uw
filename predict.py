4ebfbcb0686e774edf7de224cee3dae16d6fe00f
HDTX-code <1252900424@qq.com>
2022-05-11 16:36:51 +0800

    增加预测模块

--- predict.py
+++ predict.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 import cv2
 import numpy as np
@@ -13,8 +14,8 @@ def go_predict(args):
     print(torch.cuda.is_available())
     unet = Unet(args.model_path, args.num_classes, args.backbone, [args.w, args.h], torch.cuda.is_available())
     image = unet.detect_image(Image.open(args.pic_path), mix_type=0)
-    plt.imshow(cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR))
-    plt.show()
+    image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
+    cv2.imwrite(os.path.join(args.save_dir, 'image.jpg'), image)
 
 
 if __name__ == '__main__':
@@ -26,6 +27,7 @@ if __name__ == '__main__':
     parser.add_argument('--h', type=int, default=512, help='高')
     parser.add_argument('--mix_type', type=int, default=0, help='原图与生成的图进行混合模式')
     parser.add_argument('--pic_path', type=str, required=True, help='图片路径地址')
+    parser.add_argument('--save_dir', type=str, default="./", help='存储文件夹位置')
     args = parser.parse_args()
 
     go_predict(args)

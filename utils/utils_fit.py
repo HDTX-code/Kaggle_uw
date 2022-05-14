5c6b6130ee2041f84afe63545d9b07097e5bc2e9
HDTX <1252900424@qq.com>
2022-05-14 16:53:07 +0800

    dice_loss_weights

--- utils/utils_fit.py
+++ utils/utils_fit.py
@@ -45,8 +45,8 @@ def fit_one_epoch(model, optimizer, epoch_now, epoch_Freeze, num_classes,
 
             total_loss += loss.item()
             total_f_score += _f_score.item()
-            pbar_train.set_postfix(**{'total_loss': total_loss / (iteration + 1),
-                                      'f_score': total_f_score / (iteration + 1),
+            pbar_train.set_postfix(**{'t_l': total_loss / (iteration + 1),
+                                      'f_s': total_f_score / (iteration + 1),
                                       'lr': get_lr(optimizer)})
             pbar_train.update(1)
 
@@ -82,8 +82,8 @@ def fit_one_epoch(model, optimizer, epoch_now, epoch_Freeze, num_classes,
                     val_loss += loss.item()
                     val_f_score += _f_score.item()
 
-                    pbar_val.set_postfix(**{'val_loss': val_loss / (iteration + 1),
-                                            'f_score': val_f_score / (iteration + 1),
+                    pbar_val.set_postfix(**{'v_l': val_loss / (iteration + 1),
+                                            'f_s': val_f_score / (iteration + 1),
                                             'lr': get_lr(optimizer)})
                     pbar_val.update(1)
         # 保存模型

a1ff2b1ccf312333ac450663f9306947370b13d7
HDTX <1252900424@qq.com>
2022-05-15 16:47:59 +0800

    修改成二分类

--- utils/UNetDataset.py
+++ utils/UNetDataset.py
@@ -31,7 +31,7 @@ class UNetDataset(Dataset):
         # -------------------------------------------------------#
         seg_labels = np.eye(self.num_classes + 1)[pic_label.reshape([-1])]
         seg_labels = seg_labels.reshape((int(self.input_shape[0]), int(self.input_shape[1]), self.num_classes + 1))
-        return (pic_train- np.mean(pic_train))/255, pic_label, seg_labels
+        return pic_train/np.max(pic_train), pic_label, seg_labels
 
     @staticmethod
     def rand(a=0, b=1):
